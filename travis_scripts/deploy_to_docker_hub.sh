0989dd602b214b885d913778bf0383e5bc0f9ebb
hieuhust <hieu.fet@gmail.com>
2019-06-21 15:34:14 +0700

    only authentication-service to docker hub

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,8 +1,8 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push hieufet/tmx-authentication-service:$BUILD_NAME
-docker push hieufet/tmx-licensing-service:$BUILD_NAME
-docker push hieufet/tmx-organization-service:$BUILD_NAME
-docker push hieufet/tmx-confsvr:$BUILD_NAME
-docker push hieufet/tmx-eurekasvr:$BUILD_NAME
-docker push hieufet/tmx-zuulsvr:$BUILD_NAME
+#docker push hieufet/tmx-licensing-service:$BUILD_NAME
+#docker push hieufet/tmx-organization-service:$BUILD_NAME
+#docker push hieufet/tmx-confsvr:$BUILD_NAME
+#docker push hieufet/tmx-eurekasvr:$BUILD_NAME
+#docker push hieufet/tmx-zuulsvr:$BUILD_NAME
